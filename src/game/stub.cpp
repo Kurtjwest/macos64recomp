5e6015dd3aa367b21d552be1c322383810c5a927
Kurtjwest <34839176+Kurtjwest@users.noreply.github.com>
2025-06-07 13:07:11 -0400

    fixup

---
+++ src/game/stub.cpp
@@ -0,0 +1,24 @@
+#include <ultramodern/ultra64.h>
+#include <ultramodern/ultramodern.hpp>
+#include "recomp.h"
+
+
+extern "C" void osPfsInit_recomp(uint8_t* rdram, recomp_context* ctx) {
+	assert(false);
+}
+
+extern "C" void osPfsIsPlug_recomp(uint8_t* rdram, recomp_context* ctx) {
+	assert(false);
+}
+
+extern "C" void __osPfsRWInode_recomp(uint8_t* rdram, recomp_context* ctx) {
+	assert(false);
+}
+
+extern "C" void __osGetCurrFaultedThread(uint8_t* rdram, recomp_context* ctx) {
+	assert(false);
+}
+
+extern "C" void osSetTime(uint8_t* rdram, recomp_context* ctx) {
+	assert(false);
+}
